16eb75e83b010d5dd887d2e4eeafa6c6b103f7a9
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 20:40:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ add_selectbox = st.sidebar.selectbox(
     "Pilih menu berikut:",
     ("Beranda", "Kalkulator Faktorial", "Cek Ganjil Genap", "Tentang kami")
 )
-if add_selectbox="Beranda":
+if add_selectbox=="Beranda":
     st.write(
         '''Aplikasi ini dibuat untuk memenuhi tugas kelompok Praktik LPK 2025. Semoga dapat bermanfaat untuk 
         orang banyak'''
