3901fc74eff706336d2d18d5867cb1f4f1b5e568
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 20:34:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,8 @@ from pathlib import Path
 
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "Pilih menu berikut:",
+    ("Beranda", "Kalkulator Faktorial", "Cek Ganjil Genap", "Tentang kami")
 )
 
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+
