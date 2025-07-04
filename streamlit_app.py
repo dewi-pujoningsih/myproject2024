6703d44e8ec90ea69500f2ac0b51cebf1150029e
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 20:49:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,11 @@ import pandas as pd
 import math
 from pathlib import Path
 
-
+def faktorial(n):
+    hasil=1
+    for i in range(1,n+1):
+        hasil*=i
+    print(f"Hasil {n}! adalah {hasil}")
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     "Pilih menu berikut:",
@@ -14,4 +18,8 @@ if add_selectbox=="Beranda":
     st.markdown('''
     Aplikasi ini dibuat untuk memenuhi tugas Praktik Logika dan Pemrograman Komputer.
     Tugas disusun oleh 5 orang''')
+elif add_selectbox=="Kalkulator Faktorial":
+    angka=int(st.input("Masukkan n:"))
+    faktorial(angka)
+    
 
