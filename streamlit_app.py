9ebf8b02252cb0aa3680b2a305c0eaa6d94d02d9
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 21:05:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,18 @@ import pandas as pd
 import math
 from pathlib import Path
 
-def faktorial(n):
-    global hasil
-    hasil=1
-    for i in range(1,n+1):
-        hasil*=i
-    print(f"Hasil {n}! adalah {hasil}")
+
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     "Pilih menu berikut:",
     ("Beranda", "Kalkulator Faktorial", "Cek Ganjil Genap", "Tentang kami")
 )
+def faktorial(n):
+    global hasil
+    hasil=1
+    for i in range(1,n+1):
+        hasil*=i
+    print("Hasil:",hasil)
 if add_selectbox=="Beranda":
     st.header("**Aplikasi Aneka Fungsi**")
     st.markdown('''
