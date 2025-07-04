1906142c4f7bc11920facfe6ac1b92fbae20fc07
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 20:56:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ if add_selectbox=="Beranda":
     Aplikasi ini dibuat untuk memenuhi tugas Praktik Logika dan Pemrograman Komputer.
     Tugas disusun oleh 5 orang''')
 elif add_selectbox=="Kalkulator Faktorial":
-    st.tittle("Kalkulator Faktorial")
+    st.title("Kalkulator Faktorial")
     
 
