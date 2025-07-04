e3296878933d9e78c9b19fc7f28fd8914a030a55
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 20:45:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,8 @@ add_selectbox = st.sidebar.selectbox(
     ("Beranda", "Kalkulator Faktorial", "Cek Ganjil Genap", "Tentang kami")
 )
 if add_selectbox=="Beranda":
-    st.markdown("*Aplikasi* **Gado** ***Gado***.")
+    st.header("**Aplikasi Aneka Fungsi**")
     st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
-
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
+    Aplikasi ini dibuat untuk memenuhi tugas Praktik Logika dan Pemrograman Komputer.
+    Tugas disusun oleh 5 orang''')
 
